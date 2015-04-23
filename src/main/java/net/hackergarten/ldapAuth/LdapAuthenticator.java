826835dddbdd60462e909edbdba4a707092ffdd1
Markus Schlichting <markus.schlichting@canoo.com>
2015-04-23 19:37:38 +0200

    convert groovy to java code

--- src/main/java/net/hackergarten/ldapAuth/LdapAuthenticator.java
+++ src/main/java/net/hackergarten/ldapAuth/LdapAuthenticator.java
@@ -1,4 +1,4 @@
-package net.hackergarten.ldapAuth
+package net.hackergarten.ldapAuth;
 
 /*
  *
@@ -9,23 +9,23 @@ import javax.naming.*;
 import javax.naming.directory.*;
 
 public class LdapAuthenticator {
-    private final static String searchBase = "ou=accounts,dc=hackergarten,dc=net"
-    private final static String ldapURI = "ldap://hackergartenserver:389/"// + searchBase;
-    private final static String contextFactory = "com.sun.jndi.ldap.LdapCtxFactory"
+    private final static String searchBase = "ou=accounts,dc=hackergarten,dc=net";
+    private final static String ldapURI = "ldap://hackergartenserver:389/";// + searchBase;
+    private final static String contextFactory = "com.sun.jndi.ldap.LdapCtxFactory";
 
-      DirContext ldapContext () throws Exception {
+    private DirContext ldapContext () throws NamingException {
         Hashtable<String,String> env = new Hashtable <String,String>();
         return ldapContext(env);
     }
 
-      DirContext ldapContext (Hashtable <String,String>env) throws Exception {
+    private DirContext ldapContext (Hashtable <String,String>env) throws NamingException {
         env.put(Context.INITIAL_CONTEXT_FACTORY, contextFactory);
         env.put(Context.PROVIDER_URL, ldapURI);
         DirContext ctx = new InitialDirContext(env);
         return ctx;
     }
 
-     String getUid (String user) throws Exception {
+    public String getUid (String user) throws Exception {
         DirContext ctx = ldapContext();
 
         String filter = "(uid=" + user + ")";
@@ -45,7 +45,7 @@ public class LdapAuthenticator {
         return dn;
     }
 
-     boolean testBind (String dn, String password) throws Exception {
+    public boolean testBind (String dn, String password) throws Exception {
         Hashtable<String,String> env = new Hashtable <String,String>();
         env.put(Context.SECURITY_AUTHENTICATION, "simple");
         env.put(Context.SECURITY_PRINCIPAL, dn);
@@ -60,8 +60,8 @@ public class LdapAuthenticator {
         return true;
     }
 
-    def search(String uid){
-        def ldap = ldapContext()
+    public Map<String,String> search(String uid) throws NamingException{
+        DirContext ldap = ldapContext();
 
         SearchControls searchCtls = new SearchControls();
         searchCtls.setReturningAttributes("cn,givenName,mail".split(","));
